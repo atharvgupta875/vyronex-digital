d467d9038a3a0e9e77a4b03fd236e919ecbff2e6
Atharv Gupta <atharvgupta875@gmail.com>
2026-05-28 22:25:37 +0530

    Added sitemap

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
-import path from 'path'
+import { sitemap } from 'vite-plugin-sitemap'
 
 export default defineConfig({
   plugins: [
     react(),
-    tailwindcss(),
-  ],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
+    sitemap({
+      hostname: 'https://www.vyronexdigital.online'
+    })
+  ]
 })
